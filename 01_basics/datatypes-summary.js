a581deb68d9ec8f4a44768722f25793b2dbc78a4
Prince Mishra <princemishraa588@gmail.com>
2025-03-26 16:51:29 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,15 +35,44 @@ const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
     name: "prince",
     age: 22,
-    [id] : "165"  // If you want to print id as symbol, then you have to enclose it into []
+    [id]: "165"  // If you want to print id as symbol, then you have to enclose it into []
 }
 
 console.log(myObj);
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*
+Primitive data types are stored in a stack,
+and non-primitive data types are stored in a heap.
+
+Stack: 
+    - It's used for storing small, fixed-size data (like numbers and simple text) and for keeping track of function calls.
+    - It's very fast.
+    - Data is added and removed in a "last-in, first-out" (LIFO) manner.
+
+Heap:
+    - It's used for storing larger, more complex data (like objects and arrays) that can change in size.
+    - It's more flexible but slower than the stack.
+    - It's where dynamic memory allocation happens.
+    - Variables that hold object data, only hold a reference that points to the object location within the heap.
+*/
+
+let userOne = {
+    email: "user@mail.com",
+    upi: "test@ybl"
+}
+
+let userTwo = userOne;  // Giving refernece of userOne to userTwo
+console.log(userTwo.email);  // Output => user@mail.com
+userTwo.email = "prince@mail.com";  // It directly changes the value in reference, due to which value of "userOne.email" changes automatically.
+console.log(userOne.email);  // Output => prince@mail.com
+console.log(userTwo.email);  // Output => prince@mail.com
