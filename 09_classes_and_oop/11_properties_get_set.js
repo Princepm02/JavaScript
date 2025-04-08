be182c067e1fc3164eb38d46799c802154b17ca1
Prince Mishra <princemishraa588@gmail.com>
2025-04-08 12:43:20 +0530

    Create 11_properties_get_set.js

---
+++ 09_classes_and_oop/11_properties_get_set.js
@@ -0,0 +1,27 @@
+// Before the class keyword was introduced in JavaScript, the same task as in 10_getter_setter.js was often accomplished in this way.
+function User(email, password) {
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function () {
+            return this._email.toUpperCase()
+        },
+        set: function (value) {
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function () {
+            return this._password.toUpperCase()
+        },
+        set: function (value) {
+            this._password = value
+        }
+    })
+
+}
+
+const person = new User("prince@prince.com", "mishra")
+
+console.log(person.email);  // Output => PRINCE@PRINCE.COM
